9c072e94a3ea1075851e34bc3b7f35f80cf0992f
William Rodrigues <williamr723@gmail.com>
2024-05-15 20:01:33 -0300

    initial commit

---
+++ banco.py
@@ -0,0 +1,61 @@
+menu = """
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
+
+=> """
+
+saldo = 0
+limite = 1000
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor a ser depositado: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+   
+    elif opcao == "s":
+        valor = float(input("Informe o valor a ser sacado: "))
+
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! O valor informado é maior que o saldo atual.")
+        
+        elif excedeu_limite:
+            print("Operação falhou! O valor informado é maior que o limite de saque.")
+
+        elif excedeu_saques:
+            print("Operação falhou! O valor informado excedeu o máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == 'e':
+        print("\n ============== EXTRATO ==================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("============================================")
+    elif opcao == 'q':
+        break
+
+    else:
+        print("Operação inválida. Por favor selecione novamente a operação desejada..")
+    
